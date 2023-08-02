b40c3568f312cf700b6e1b40b2a7a19252fea98d
zac2222 <zacrice2222@gmail.com>
2023-08-01 19:57:23 -0700

    special feature changes colors of board state

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 interface SquareProps{
     value: string | null;
+    color: string;
     onSquareClick?: React.MouseEventHandler<HTMLButtonElement>; //sets it to just work as an onclick button default.
 }
 
@@ -11,14 +12,15 @@ interface BoardProps {
     onPlay: (nextSquares: (string | null)[]) => void;
 }
 
-function Square({value,onSquareClick}: SquareProps)
+function Square({value,color,onSquareClick}: SquareProps)
 {
-    return <button className='square' onClick={onSquareClick}>{value}</button>
+    return <button className='square' style={{backgroundColor: color}} onClick={onSquareClick}>{value}</button>
 }
 
 function Board({xIsNext,squares,onPlay}:BoardProps) {
     // const [xIsNext, setXIsNext] = useState(true)
     // const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null)); 
+    const [colors, setColors] = useState<string[]>(Array(9).fill('white'));
 
     function handleClick(i: number)
     {
@@ -28,47 +30,59 @@ function Board({xIsNext,squares,onPlay}:BoardProps) {
         }
 
         const nextSquares = squares.slice();
+        const nextColors = colors.slice();
         if(xIsNext)
         {
             nextSquares[i] = 'X';
+            nextColors[i] = 'blue';
         }
         else
         {
             nextSquares[i] = 'O'
+            nextColors[i] = 'violet';
         }
         onPlay(nextSquares)
+        setColors(nextColors);
     }
 
     const winner = calulateWinner(squares);
     let status;
-    if(winner)
+    if(winner === 'X')
     {
         status = 'Winner: ' + winner;
+        document.body.classList.add('winner-x');
+    }
+    else if(winner === 'O')
+    {
+        status = 'Winner: ' + winner;
+        document.body.classList.add('winner-o');
     }
     else
     {
         status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+        document.body.classList.remove('winner-x');
+        document.body.classList.remove('winner-o');
     }
 
   return (
     <>
     <div className='status'>{status}</div>
         <div className='board-row'>
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            <Square value={squares[0]} color={colors[0]} onSquareClick={() => handleClick(0)}/>
+            <Square value={squares[1]} color={colors[1]} onSquareClick={() => handleClick(1)}/>
+            <Square value={squares[2]} color={colors[2]} onSquareClick={() => handleClick(2)}/>
         </div>
 
         <div className='board-row'>
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            <Square value={squares[3]}  color={colors[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]}  color={colors[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]}  color={colors[5]} onSquareClick={() => handleClick(5)}/>
         </div>
 
         <div className='board-row'>
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            <Square value={squares[6]} color={colors[6]}  onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} color={colors[7]}  onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} color={colors[8]}  onSquareClick={() => handleClick(8)}/>
         </div>
  
     </>
@@ -91,7 +105,7 @@ const Game = () => {
         setCurrentMove(nextHistory.length - 1);
     }
 
-    function jumpTo(nextMove: (number)){ //RIGHT HERE, HERE IS WHERE YOU FUCKED UP COME MAKE SURE ITS OKAY. STUPID
+    function jumpTo(nextMove: (number)){ 
         setCurrentMove(nextMove);
     }
 
@@ -122,6 +136,9 @@ const Game = () => {
             </div>
         </div>
     )
+
+    //left the colors the same on purpose after going back to previous moves.
+    //i thought being able to see the colored squares of where each one went and then being able to write over them is a cool feature.
 }
 
 
